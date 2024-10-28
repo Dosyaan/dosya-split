621cfb73cc9baa523696246789fcea37819da8b5
Dosyaan <dastanmatraimov79@gmail.com>
2024-10-28 18:01:36 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,76 @@
+// task - 1
+
+// const hello = 'Hello, how are you?'
+// const arr = hello.split(' ')
+
+// console.log(arr)
+
+
+// task - 2
+
+// const fruit =  "яблоко,банан,груша,апельсин"
+// const arr = fruit.split('')
+
+// console.log(arr)
+
+
+// task - 3
+
+// const hello = ["Привет", "мир", "!"]
+// const arr = hello.join(' ')
+
+// console.log(arr)
+
+// task - 4
+
+// const hello = "hello";
+// const arr = hello.split('').reverse('').join('');
+
+// console.log(arr);
+
+
+// task - 5
+
+// const text = "Каждое слово начинается с буквы"
+// const first = []
+// const arr = text.split(' ')
+
+// arr.forEach(item =>{
+//     first.push(item[0])
+// })
+
+// console.log(first)
+
+// task - 6
+
+// const text = "Привет как дела"
+// const arr = text.split(' ').join('-')
+
+// console.log(arr)
+
+// task - 7
+
+// const text = "JavaScript - это весело"
+// const num = text.split(' ').length
+
+// console.log(num)
+
+
+// task - 8
+
+// const text = "   Привет   как    дела   "
+// const del = text.split('   ').join(' ')
+
+// console.log(del)
+
+// task - 9
+
+// const num = [1, 2, 3, 4, 5]
+// const arr = num.join(',')
+
+// console.log(arr)
+
+// task - 10
+
+const text = "1 2 3 4 5"
+const 
